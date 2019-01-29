bba42efdd7b756f638e5322bf20e68da193d0fcd
David Hanks <dphanks@gmail.com>
2019-01-29 14:11:13 -0600

    heroku configs

--- src/components/NewPanel.js
+++ src/components/NewPanel.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 
 class NewPanel extends Component{
-    constructor(props){
-        super(props);
-    }    
     render(){
         return(
             <div className="panel">
